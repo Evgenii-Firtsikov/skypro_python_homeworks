c41067a30f36f1d3d37dcec1376211dc1572e593
Evgenii.F <163299361+Evgenii-Firtsikov@users.noreply.github.com>
2024-08-25 17:49:39 +0300

    add first homework

---
+++ lesson_3/lesson_3_task_2.py
@@ -0,0 +1,12 @@
+from smartphone import Smartphone
+
+catalog = [
+    Smartphone('Samsung', 'Galaxy S21', '+11111111111'),
+    Smartphone('Apple', 'iPhone 13', '+22222222222'),
+    Smartphone('Xiaomi', 'Redmi Note 11', '+33333333333'),
+    Smartphone('Sony', 'Xperia 10', '+44444444444'),
+    Smartphone('Huawei', 'P40 Pro', '+55555555555')  
+]
+
+for phone in catalog:
+    print(phone)
